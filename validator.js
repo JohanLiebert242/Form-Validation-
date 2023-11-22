729c9369775765d805e1bc2be9a56b53c2960a12
JohanLiebert <nguyenloc24022005@gmail.com>
2023-11-22 23:39:00 +0700

    Handle Basic Functions

--- validator.js
+++ validator.js
@@ -1,61 +1,138 @@
-function Validator(formSelector) {
-    var formArrayRules = {};
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-    //Tạo ra các rules
-    var formRules = {
-        required: function(value) {
-            return value ? undefined : 'Vui lòng nhập trường này';
+function Validator(formSelector, options = {}) {
+
+    var rulesPushed = {};
+
+    //Hàm GetParent
+    function getParent(element, selector) {
+        while(element.parentElement) {
+            if(element.parentElement.matches(selector)) {
+                return element.parentElement;
+            }
+            element = element.parentElement;  
+        }
+    }
+
+    //Form Rules
+    var formRules = {  
+        required(value) {
+            return value ? undefined : 'Vui lòng nhập thông tin của bạn';
         },
 
-        email: function(value) {
+        email(value) {
             var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return emailRegex.test(value) ? undefined : 'Vui lòng nhập email';
+            return emailRegex.test(value) ? undefined : 'Vui lòng nhập email của bạn';
         },
-        
+
         min: function(min) {
             return function(value) {
-                return value.length >= min ? undefined : `Vui lòng nhập tối thiểu ${min} kí tự`;
+                return value.length >= min ? undefined : `Vui lòng nhập ít nhất ${min} kí tự `;
             }
         }
     }
 
-    //Lấy ra thẻ form
-    var formElement = document.querySelector(formSelector);
+    //Handle rules
+    handleEvent = function(e) {
+        var rules = rulesPushed[e.target.name];
+        var errorMessage;
 
-    if(formElement) {
-        //Lấy ra tất cả thẻ inputs
-        var inputs = formElement.querySelectorAll('[name][rules]');
-        for(var input of inputs) {
+        rules.some((rule) => {
+            switch(input.type) {}
+            errorMessage = rule(e.target.value);
+            return errorMessage;
+        })
 
-            // Tách các rules có dấu | thành mảng
-            var rules = input.getAttribute('rules').split('|');
-            for(var rule of rules) {
-                var isColon = rule.includes(':');
-
-                //Kiểm tra xem có dấu : hay không
-                if(isColon) {
-                    var ruleContainingColon = rule.split(':');
-                    rule = ruleContainingColon[0];
-                }  
-                
-                var ruleFunction = formRules[rule];
-                if(isColon) {
-                    ruleFunction = ruleFunction(ruleContainingColon[1]);
+        if(errorMessage) {
+            var formGroup = getParent(e.target, '.form-group');
+            if(formGroup) {
+                formGroup.classList.add('invalid');
+                var formMessage = formGroup.querySelector('.form-message');
+                if(formMessage) {
+                    formMessage.innerText = errorMessage;
                 }
+            }
+        }
 
-                //Đẩy các phần tử vào mảng trống
-                if(Array.isArray(formArrayRules[input.name])) {
-                    formArrayRules[input.name].push(ruleFunction);
+        return !errorMessage;
+    }
+    
+    //Handle xóa lỗi
+    handleClearError = function(e) {
+        var formGroup = getParent(e.target, '.form-group');
+            if(formGroup) {
+                formGroup.classList.remove('invalid');
+                var formMessage = formGroup.querySelector('.form-message');
+                if(formMessage) {
+                    formMessage.innerText = '';
+                }
+            }
+    }
+
+    //Handle FormSubmit
+    handleFormSubmit = function(e) {
+        e.preventDefault();
+
+        var inputElement = formElement.querySelectorAll('[name][rules]');
+        for(var input of inputElement) {
+
+            var isValid = true;
+            if(!handleEvent({target: input})) {
+                isValid = false;
+            }
+
+            if(isValid) {
+                if(typeof options.onSubmit === 'function') {
+                    var enabledInputs = formElement.querySelectorAll('[name]');
+                    var formValues = Array.from(enabledInputs).reduce(function(values, input) {
+                        values[input.name] = input.value;
+                        return values;
+                    },  {});
+                    options.onSubmit(formValues);
                 }
                 else {
-                    formArrayRules[input.name] = [ruleFunction];
+                    formElement.submit();
                 }
-
             }
-            
-            
         }
     }
-    console.log(formArrayRules);
-}
 
+  
+
+    //Logic 
+    var formElement = $(formSelector);
+    if(formElement) {
+        var inputElement = formElement.querySelectorAll('[name][rules]');
+        for(var input of inputElement) {
+            var rules = input.getAttribute('rules').split('|');
+            for(var rule of rules) {
+
+
+                var ruleHasColon;
+                var isColon = rule.includes(':');
+                if(isColon) {
+                    ruleHasColon = rule.split(':');
+                    rule = ruleHasColon[0];
+                }
+                
+                var ruleFunctions = formRules[rule];
+                if(isColon) {
+                    ruleFunctions = ruleFunctions(ruleHasColon[1]);
+                }
+                if(Array.isArray(rulesPushed[input.name])) {
+                    rulesPushed[input.name].push(ruleFunctions);
+                }   
+                else {
+                    rulesPushed[input.name] = [ruleFunctions];
+                }
+            }
+
+            //Events
+            input.onblur = handleEvent;
+            input.oninput = handleClearError;
+        }
+        formElement.onsubmit = handleFormSubmit;           
+
+    }
+}
